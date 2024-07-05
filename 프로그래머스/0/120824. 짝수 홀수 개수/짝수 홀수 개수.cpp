32460bfe7d2601e691c0684c40f4b1588b4076e4
TSATOA <53053131+TSATOA@users.noreply.github.com>
2024-07-05 12:48:17 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    answer.push_back(0);
+    answer.push_back(0);
+    for(int i=0;i<num_list.size();i++){
+        if(num_list[i]%2==0)
+            answer[0]++;
+        else
+            answer[1]++;
+    }
+    return answer;
+}
