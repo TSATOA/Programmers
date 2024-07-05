5d31e2340f4d90c753b62abc3557001c9c4f58b0
TSATOA <53053131+TSATOA@users.noreply.github.com>
2024-07-05 16:38:38 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for(int i=0;i<my_string.size();i++){
+        if(my_string[i]>='a'&&my_string[i]<='z'){
+            my_string[i]-=32;
+        }else if(my_string[i]>='A'&&my_string[i]<='Z'){
+            my_string[i]+=32;
+        }
+        answer.push_back(my_string[i]);
+    }
+    return answer;
+}
