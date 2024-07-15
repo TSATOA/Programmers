0cf32d6bc0c4031af0869f982a86d67b77b94ca9
TSATOA <53053131+TSATOA@users.noreply.github.com>
2024-07-15 13:40:15 +0900

    [level 2] Title: 올바른 괄호, Time: 0.56 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,23 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = false;
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        if(!st.empty()&&st.top()!=s[i]&&st.top()=='('){
+            st.pop();
+        }else{
+            st.push(s[i]);
+        }
+        
+    }
+
+    if(st.empty())
+        answer = true;
+    return answer;
+}
